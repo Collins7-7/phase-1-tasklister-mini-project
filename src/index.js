22915ff9ec82986fb8ad9fdfb00aff9034d8312c
Collins Muiruri Kamau <collins.muiruri@student.moringaschool.com>
2022-08-24 19:06:31 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const li = document.createElement("li");
+    const userInput = document.getElementById("new-task-description");
+    li.textContent = userInput.value;
+    let ul = document.getElementById('tasks');
+    ul.append(li);
+    const button = document.createElement('button');
+    button.innerText = 'X'
+    const div = document.getElementById('list');
+    li.appendChild(button);
+    button.addEventListener('click', () => {
+      li.remove();
+    });
+  });
 });
